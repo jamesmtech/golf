d81ef496298d51fc84d4f69470b7809e4d5b49e3
James Barlow <james.barlow0725@stu.mtec.edu>
2021-03-26 10:00:01 -0600

    message

--- summons.js
+++ summons.js
@@ -1,26 +1,49 @@
-function getAvailableCourses() {
+async function getAvailableCourses() {
  return fetch('https://golf-courses-api.herokuapp.com/courses/').then(
    function (response) {
-     return response.json();
+     let data =response.json()
+     return data;
    }
  );
 }
+async function getCourseInfo(courseId) {
+  fetch('https://golf-courses-api.herokuapp.com/courses/' +courseId).then(
+   response => displayCourse (response) 
+ );
+}
+getAvailableCourses().then(data=>{
 
-getAvailableCourses();
+  const courseSelect = document.getElementById("course-select")
+  console.log(data)
 
-let courseOptionsHtml = '';
-courses.forEach((course) => {
- courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
+  data.courses.forEach(function(value){
+    courseSelect.innerHTML+= `<option value="${value.id}">${value.name}</option>`
+  })
+
 });
-document.getElementById('course-select').innerHTML = courseOptionsHtml;
+
+function displayCourse (response) {
+     let course =response.json()
+     console.log(course);
+     document.getElementById("yardage1").innerHTML=course.courseId;
+    //  document.getElementById("yardage1").innerHTML=course.data.holes[0].teeBoxes[0].yards;
+   }
 
 
 
-let teeBoxSelectHtml = ''
-teeBoxes.forEach(function (teeBox, index) {
-   teeBoxSelectHtml += `<option value="${index}">${teeBox.teeType.toUpperCase()}, ${
-     teeBox.totalYards
-   } yards</option>`
-});
-document.getElementById('tee-box-select').innerHTML = teeBoxSelectHtml;
+// let courseOptionsHtml = '';
+// courses.forEach((course) => {
+//  courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
+// });
+// document.getElementById('course-select').innerHTML = courseOptionsHtml;
+
+
+
+// let teeBoxSelectHtml = ''
+// teeBoxes.forEach(function (teeBox, index) {
+//    teeBoxSelectHtml += `<option value="${index}">${teeBox.teeType.toUpperCase()}, ${
+//      teeBox.totalYards
+//    } yards</option>`
+// });
+// document.getElementById('tee-box-select').innerHTML = teeBoxSelectHtml;
 
